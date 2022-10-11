7f0e7bd02b128d34b3354583a37ce3e5cecb702d
jotaeme890 <permanentjavi@gmail.com>
2022-10-11 19:08:37 +0200

    Ejercicio7

--- Ejercicio7.java
+++ Ejercicio7.java
@@ -18,12 +18,14 @@ public class Ejercicio7 {
             System.out.print("Dime la nota del tercer examen: ");
             float  n3 = sc.nextFloat();
 
+            float media = ((n+n2+n3)/3);
+
             if(n < 0 || n2 < 0 || n3 < 0){
                 System.out.print("Por favor, dime bien los datos");
             } else if(n > 10 || n2 > 10 || n3 > 10){
                 System.out.print("Por favor, dime bien los datos");
             }else
-                System.out.print("La media sería: " + ((n+n2+n3)/3));
+                System.out.printf("La media es %.2f\n", media);
 
             
         
