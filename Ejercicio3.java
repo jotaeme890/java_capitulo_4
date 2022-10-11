dfad918ce0107b0dd93c571bfe26fcdad0c8e337
jotaeme890 <permanentjavi@gmail.com>
2022-10-11 12:54:29 +0200

    Ejercicio3

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -15,31 +15,31 @@ public class Ejercicio3{
 
         switch(n) {
             case 1:
-            System.out.println("Este número corresponde al Lunes");
+                System.out.println("Este número corresponde al Lunes");
             break;
 
             case 2:
-            System.out.println("Este número corresponde al Martes");
+                System.out.println("Este número corresponde al Martes");
             break;
 
             case 3:
-            System.out.println("Este número corresponde al Miércoles");
+                System.out.println("Este número corresponde al Miércoles");
             break;
 
             case 4:
-            System.out.println("Este número corresponde al Jueves");
+                System.out.println("Este número corresponde al Jueves");
             break;
 
             case 5:
-            System.out.println("Este número corresponde al Viernes");
+                System.out.println("Este número corresponde al Viernes");
             break;
 
             case 6:
-            System.out.println("Este número corresponde al Sábado");
+                System.out.println("Este número corresponde al Sábado");
             break;
 
             case 7:
-            System.out.println("Este número corresponde al Miércoles");
+                System.out.println("Este número corresponde al Miércoles");
             break;
         }
         sc.close();
