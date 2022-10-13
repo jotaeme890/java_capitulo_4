82b5e5febb58d3e6f9c28db6724e6cef2b6e0489
jotaeme890 <permanentjavi@gmail.com>
2022-10-13 09:16:17 +0200

    Ejercicio6

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -1,5 +1,5 @@
 /*
- * 1 Realiza un programa que calcule el tiempo que tardará en caer un objeto desde 
+ * 6 Realiza un programa que calcule el tiempo que tardará en caer un objeto desde 
  * una altura h. Aplica la fórmula t =√2h/g siendo g = 9.81m/s2
  * 
  * @author jotaeme
