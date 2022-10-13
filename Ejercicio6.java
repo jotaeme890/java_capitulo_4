4e09e89722a74b4a6dab62b9b6a2ba0108824856
jotaeme890 <permanentjavi@gmail.com>
2022-10-13 08:26:25 +0200

    Ejercicio6

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -5,13 +5,11 @@
  * @author jotaeme
  */
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.lang.Math;
 
 public class Ejercicio6 {
     public static void main(String args []){
-        try {
             Scanner sc = new Scanner (System.in);
             System.out.print("Dime el valor de la altura en metros: ");
             float  h = sc.nextFloat();
@@ -22,9 +20,5 @@ public class Ejercicio6 {
                 System.out.print("Tradaría un total de " + Math.sqrt(2*h/9.81) + " segundos");
         
         sc.close();
-
-        } catch (InputMismatchException ex) {
-            System.out.println("Por favor dime bien los formatos");
-        }  
     } 
 }
