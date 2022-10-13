5e21509ea866dbf9c9927166da3dab5425a1c045
jotaeme890 <permanentjavi@gmail.com>
2022-10-13 09:16:00 +0200

    Ejercicio3

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,5 +1,5 @@
 /*
- * 1 Escribe un programa en que dado un número del 1 a 7 escriba el correspondiente 
+ * 3 Escribe un programa en que dado un número del 1 a 7 escriba el correspondiente 
  * nombre del día de la semana
  * 
  * @author jotaeme
